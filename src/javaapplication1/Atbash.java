16fb39a537ad797e047ac739db2a60d6b1338656
Cesar Samanez J <csamanezj@unsa.edu.pe>
2022-05-12 12:00:37 -0500

    App

---
+++ src/javaapplication1/Atbash.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/*
+ * Algoritmo de descifrado Atbash
+ * El cifrado de este algoritmo consiste en invertir las letras de la A-Z y de la Z-A
+ * [ABCDEF] = [ZYXWVU]
+ */
+
+public class Atbash {
+	/*
+	 * Metodo general para descifrar texto
+	 */
+	public String descifrarTextoAtbash(String texto) {
+		String resultadoTextoDescifrado = ""; // Variable para almacenar el resultado
+		texto = texto.toUpperCase(); // Convertir el texto a mayusculas
+
+		// Iterar cada letra del texto
+		for (char c : texto.toCharArray()) {
+			if (Character.isLetter(c)) { // Sustituir por su letra contraria
+				resultadoTextoDescifrado += (char) ('Z' + ('A' - c));
+			} else {
+				resultadoTextoDescifrado += c;
+			}
+		}
+
+		// Retornar el texto descifrado
+		return resultadoTextoDescifrado;
+	}
+}
